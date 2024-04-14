680ceb5051200876f5d14a813ed30bcf65b1bf98
Radzhan <rbayhombaev@gmail.com>
2024-04-14 13:32:12 +0600

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-
+import { Route, Routes } from "react-router-dom";
+import Main from "./screens/Main/Main";
 
 function App() {
-  return <div>
-  </div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<h2>Not Found !</h2>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
