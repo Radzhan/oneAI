d82d3913d1fc5bbacbf83628ee046ebdea42b073
Radzhan <rbayhombaev@gmail.com>
2024-04-15 09:31:56 +0600

    add Main

--- frontend/src/screens/Main/Main.tsx
+++ frontend/src/screens/Main/Main.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Card from "../../components/Card/Card";
-import './Main.scss'
+import "./Main.scss";
 import Footer from "../../components/Footer/Footer";
+import { useAppDispatch, useAppSelector } from "../../app/GlobalStore/hooks";
+import { Cards } from "../../store/OpenAISlice";
+import { getCards } from "../../store/OpenAIThunks";
 
 const Main = () => {
+  const arrayWithCards = useAppSelector(Cards);
+  const dispatch = useAppDispatch();
+
+  const requestCards = useCallback(async () => {
+    await dispatch(getCards());
+  }, [dispatch]);
+
+  useEffect(() => {
+    requestCards().catch(console.error);
+  }, [requestCards]);
+
+  const createCard = arrayWithCards.map((element) => {
+    return <Card key={element._id} name={element.name} src={element.image} />;
+  });
+
   return (
     <>
       <Navbar />
-      <div className="card-group container">
-        <Card
-          name="Lego"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPdwBr_ofrA-xwBLBU5WIFBgn23fEikUq72w&s"
-        />
-        <Card
-          name="Lego"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPdwBr_ofrA-xwBLBU5WIFBgn23fEikUq72w&s"
-        />
-      </div>
-      <Footer/>
+      <div className="card-group container">{createCard.length === 0 ? 'нет карточек' : createCard}</div>
+      <Footer />
     </>
   );
 };
