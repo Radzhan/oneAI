446a58e3185f1c0e6b10074020b5eebaca5a322d
Radzhan <rbayhombaev@gmail.com>
2024-04-15 09:32:30 +0600

    add Card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import "./Card.scss";
 
 interface Props {
@@ -6,8 +7,10 @@ interface Props {
 }
 
 const Card: React.FC<Props> = ({ src, name }) => {
+    const navigate = useNavigate()
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => navigate('/result')}>
         <img className="card-image" src={src} alt="img" />
       <h3 className="name">{name}</h3>
     </div>
