a90747ab639941c9a30850f5959313fb32ef0ddd
Radzhan <rbayhombaev@gmail.com>
2024-04-15 09:32:16 +0600

    add Result page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./screens/Main/Main";
+import Result from "./screens/Result/Result";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/result" element={<Result />} />
         <Route path="*" element={<h2>Not Found !</h2>} />
       </Routes>
     </>
---
+++ frontend/src/screens/Result/Result.tsx
@@ -0,0 +1,49 @@
+import "./Result.scss";
+import { MdArrowBackIos } from "react-icons/md";
+import { GrGallery } from "react-icons/gr";
+import { HiOutlineCamera } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
+const Result = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="container">
+      <div className="result">
+        <button className="go-back-btn" onClick={() => navigate("/")}>
+          <MdArrowBackIos
+            style={{
+              color: "#000000",
+            }}
+          />
+          Go back
+        </button>
+        <div className="result-photo-block">
+          <img
+            src="https://i.pinimg.com/originals/57/61/5b/57615b8c0092a66c1d4058b1692955cc.gif"
+            alt="gif"
+          />
+          <div className="result-btn-group">
+            <button className="gallery-btn">
+              <GrGallery
+                style={{
+                  color: "#ffffff",
+                }}
+              />
+              Choose photo from gallery
+            </button>
+            <button className="camera-btn">
+              <HiOutlineCamera
+                style={{
+                  color: "#ffffff",
+                }}
+              />
+              Take photo with camera
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
