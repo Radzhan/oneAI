981d64e6db29bf1db8dc95584762af599c0c144e
Radzhan <rbayhombaev@gmail.com>
2024-04-15 12:23:23 +0600

    add fonts add route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./screens/Main/Main";
 import Result from "./screens/Result/Result";
+import Upload from "./screens/Upload/Upload";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/upload" element={<Upload />} />
         <Route path="*" element={<h2>Not Found !</h2>} />
       </Routes>
     </>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,21 +2,17 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ThemeProvider } from "@mui/material";
-import theme from "./theme";
 import { store } from "./app/GlobalStore/store";
 
-import './app/styles/index.scss'
+import "./app/styles/index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
